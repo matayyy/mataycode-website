b638c3653a966ce33be9d128e6a5fc6b30b127d8
matay <mat.mielewczyk@gmail.com>
2025-01-06 18:01:29 +0100

    Crated integration test. Fixed issue with integration tests running during mvm test phase instead of package. Added plugins to run unit and integration tests independently. Added plugin for starting integration tests at random port.

---
+++ src/test/java/com/mataycode/journey/CustomerIntegrationTest.java
@@ -0,0 +1,209 @@
+package com.mataycode.journey;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import com.mataycode.customer.Customer;
+import com.mataycode.customer.CustomerRegistrationRequest;
+import com.mataycode.customer.CustomerUpdateRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CustomerIntegrationTest {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    private static final Random RANDOM = new Random();
+    private static final String CUSTOMER_URI = "/api/v1/customers";
+
+    @Test
+    void canRegisterACustomer() {
+        // create registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + "-" + UUID.randomUUID() + "@journeyTest.com";
+        int age = RANDOM.nextInt(1, 100);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, age);
+
+        // send a post request
+        webClient.post()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus().isOk();
+
+        //get all customers
+        List<Customer> allCustomers = webClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        //make sure that customer is present
+        Customer expectedCustomer = new Customer(name, email, age);
+
+        assertThat(allCustomers).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .contains(expectedCustomer);
+
+        var id = allCustomers.stream()
+                .filter(customer -> customer.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        expectedCustomer.setId(id);
+
+        //get customer by id
+        webClient.get()
+                .uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<Customer>() {
+                })
+                .isEqualTo(expectedCustomer);
+    }
+
+    @Test
+    void canDeleteCustomer() {
+        // create registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + "-" + UUID.randomUUID() + "@journeyTest.com";
+        int age = RANDOM.nextInt(1, 100);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, age);
+
+        // send a post request
+        webClient.post()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus().isOk();
+
+        //get all customers
+        List<Customer> allCustomers = webClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        var id = allCustomers.stream()
+                .filter(customer -> customer.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        //delete customer
+        webClient.delete()
+                .uri(CUSTOMER_URI + "/" + id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        //get customer by id
+        webClient.get()
+                .uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+
+    @Test
+    void canUpdateCustomer() {
+        // create registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + "-" + UUID.randomUUID() + "@journeyTest.com";
+        int age = RANDOM.nextInt(1, 100);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, age);
+
+        // send a post request
+        webClient.post()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus().isOk();
+
+        //get all customers
+        List<Customer> allCustomers = webClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        var id = allCustomers.stream()
+                .filter(customer -> customer.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        //update customer
+        String newEmail = fakerName.lastName() + "-" + UUID.randomUUID() + "@journeyTest.com";
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest("updatedName", newEmail, 23);
+
+        webClient.put()
+                .uri(CUSTOMER_URI + "/" + id)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(updateRequest), CustomerUpdateRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        //get customer by id
+        Customer updatedCustomer = webClient.get()
+                .uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Customer.class)
+                .returnResult()
+                .getResponseBody();
+
+        Customer expected = new Customer(id, updateRequest.name(), updateRequest.email(), updateRequest.age());
+
+        assertThat(updatedCustomer).isEqualTo(expected);
+    }
+}
