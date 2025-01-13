1ae5cd77fc5b82e2fc503a4864793d61966e143a
matay <mat.mielewczyk@gmail.com>
2025-01-13 23:31:22 +0100

    added Chakra UI for creating frontend using templates and ui components. Adjusted template for our project.

--- frontend/react/README.md
+++ frontend/react/README.md
@@ -1,3 +1,11 @@
+USING CHAKRA UI v2 - https://v2.chakra-ui.com/getting-started
+
+npm create vite@latest
+cd react
+npm install
+npm i @chakra-ui/react@2 @emotion/react @emotion/styled framer-motion
+npm i axios
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,5 +1,23 @@
-function App() {
-    return <h1>Hello React</h1>
+import SidebarWithHeader from "./shared/SideBar.jsx";
+import {Button} from "@chakra-ui/react";
+import {useEffect} from "react";
+import {getCustomers} from "./services/client.js";
+
+const App = () => {
+
+    useEffect(() => {
+        getCustomers().then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    }, []);
+
+    return (
+        <SidebarWithHeader>
+            <Button colorScheme='teal' variant='outline'>CLICK ME!</Button>
+        </SidebarWithHeader>
+    )
 }
 
 export default App
