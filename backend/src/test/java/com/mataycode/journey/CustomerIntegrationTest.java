f8ba82d152e8f45d3e6c7cd1b5b24d039646a2df
matay <mat.mielewczyk@gmail.com>
2025-02-11 18:43:30 +0100

    created new integration tests and fake s3 client implementation to it. Added dev profile.

--- backend/src/test/java/com/mataycode/journey/CustomerIntegrationTest.java
+++ backend/src/test/java/com/mataycode/journey/CustomerIntegrationTest.java
@@ -7,11 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.shaded.com.google.common.io.Files;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
@@ -280,5 +286,110 @@ public class CustomerIntegrationTest {
         CustomerDTO expected = new CustomerDTO(id, updateRequest.name(), email, updateRequest.gender(), updateRequest.age(), List.of("ROLE_USER"), email, null);
 
         assertThat(updatedCustomer).isEqualTo(expected);
+    }
+
+    @Test
+    void canUploadAndDownloadProfilePicture() throws IOException {
+        // create registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + "-" + UUID.randomUUID() + "@journeyTest.com";
+        int age = RANDOM.nextInt(1, 100);
+
+        Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, "password", age, gender);
+
+        // send a post request
+        String jwtToken = webClient.post()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Void.class)
+                .getResponseHeaders()
+                .get(HttpHeaders.AUTHORIZATION)
+                .get(0);
+
+        //get all customers
+        List<CustomerDTO> allCustomers = webClient.get()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<CustomerDTO>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+//        var id = allCustomers.stream()
+//                .filter(customer -> customer.email().equals(email))
+//                .map(CustomerDTO::id)
+//                .findFirst()
+//                .orElseThrow();
+
+        //get customer by email
+        CustomerDTO customerDTO = webClient.get()
+                .uri(CUSTOMER_PATH + "/email/{email}", email)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<CustomerDTO>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        var id = customerDTO.id();
+
+        assertThat(customerDTO.profileImageId()).isNullOrEmpty();
+
+        Resource image = new ClassPathResource("%s.jpg".formatted(gender.name().toLowerCase()));
+        MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+        bodyBuilder.part("file", image);
+
+        webClient.post()
+                .uri(CUSTOMER_PATH + "/{customerId}/profile-image", id)
+                .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        //profile image id should be populated
+        String profileImageId = webClient.get()
+                .uri(CUSTOMER_PATH + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<CustomerDTO>() {
+                })
+                .returnResult()
+                .getResponseBody()
+                .profileImageId();
+
+        assertThat(profileImageId).isNotBlank();
+
+        //download picture for customer
+        byte[] downloadedImage = webClient.get()
+                .uri(CUSTOMER_PATH + "/{customerId}/profile-image", id)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(byte[].class)
+                .returnResult()
+                .getResponseBody();
+
+        byte[] actual = Files.toByteArray(image.getFile());
+
+        assertThat(actual).isEqualTo(downloadedImage);
     }
 }
